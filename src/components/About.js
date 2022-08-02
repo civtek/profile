2eb836dc3f38979e07ff1e6f0b9a42a23ca28db6
vanneswijaya <vannesoey@gmail.com>
2022-08-03 00:31:35 +0800

    tailwind setup and pages setup

---
+++ src/components/About.js
@@ -0,0 +1,7 @@
+export const About = () => {
+  return (
+    <div className="w-full h-screen bg-dark text-white">
+      <div className="font-black text-9xl">About page</div>
+    </div>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Jost", ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
+      colors: {
+        dark: "#0E1012",
+        light: "#A0AABA",
+        primary: "#44AF69",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      padding: ["hover", "group-hover", "even", "odd"],
+      height: ["hover", "group-hover"],
+      width: ["hover", "group-hover"],
+      margin: ["hover", "group-hover"],
+      borderWidth: ["hover", "group-hover"],
+      fontWeight: ["hover", "group-hover"],
+      transform: ["hover", "group-hover"],
+      scale: ["hover", "group-hover"],
+      translate: ["hover", "group-hover"],
+      inset: ["hover", "group-hover"],
+      space: ["hover", "group-hover"],
+      transitionDelay: ["hover", "group-hover"],
+      backgroundColor: ["even", "odd", "active"],
+      flexDirection: ["even", "odd"],
+      textColor: ["even", "odd"],
+      backgroundImage: ["first", "last", "even", "odd"],
+      gradientColorStops: ["first", "last", "even", "odd"],
+    },
   },
   plugins: [],
 };
