1dee4a35fc01e56096fcb48600933995655f1fed
vanneswijaya <vannesoey@gmail.com>
2022-08-03 23:19:15 +0800

    team and what we do done

---
+++ src/components/Team.js
@@ -0,0 +1,328 @@
+export const Team = () => {
+  return (
+    <div className="bg-dark w-full h-full p-20 text-white flex flex-col items-center">
+      <div className="text-7xl font-semibold mb-5">Our Team</div>
+      <div className="text-4xl font-medium mb-10 text-center">
+        We are a team of <span className="text-primary">diverse</span> talents
+        who <span className="text-primary">work together as one</span>
+        <br /> to build our <span className="text-primary">community</span>
+      </div>
+      <div className="grid grid-rows-2 gap-y-5">
+        <div className="grid grid-cols-5 gap-x-3">
+          <div className="flex flex-col items-center">
+            <div
+              className="
+                  relative
+                  w-56
+                  h-64
+                  shadow-lg
+                  bg-vannes bg-cover bg-left-top
+                  cursor-pointer
+                  group
+                  mb-5
+                "
+            >
+              <div
+                className="
+                    bg-primary
+                    absolute
+                    opacity-30
+                    w-full
+                    h-full
+                    inset-0
+                    group-hover:opacity-0
+                    transition
+                    duration-500
+                    ease-in-out
+                  "
+              ></div>
+            </div>
+            <div className="font-bold">Vannes Wijaya</div>
+            <div>Chief Director</div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div
+              className="
+                  relative
+                  w-56
+                  h-64
+                  shadow-lg
+                  bg-vannes bg-cover bg-left-top
+                  cursor-pointer
+                  group
+                  mb-5
+                "
+            >
+              <div
+                className="
+                    bg-primary
+                    absolute
+                    opacity-30
+                    w-full
+                    h-full
+                    inset-0
+                    group-hover:opacity-0
+                    transition
+                    duration-500
+                    ease-in-out
+                  "
+              ></div>
+            </div>
+            <div className="font-bold">Seivabel Jessica Halim</div>
+            <div>External Director</div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div
+              className="
+                  relative
+                  w-56
+                  h-64
+                  shadow-lg
+                  bg-un bg-cover bg-bottom
+                  cursor-pointer
+                  group
+                  mb-5
+                "
+            >
+              <div
+                className="
+                    bg-primary
+                    absolute
+                    opacity-30
+                    w-full
+                    h-full
+                    inset-0
+                    group-hover:opacity-0
+                    transition
+                    duration-500
+                    ease-in-out
+                  "
+              ></div>
+            </div>
+            <div className="font-bold">Enryl Einhard</div>
+            <div>Internal Director</div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div
+              className="
+                  relative
+                  w-56
+                  h-64
+                  shadow-lg
+                  bg-peve bg-cover bg-bottom
+                  cursor-pointer
+                  group
+                  mb-5
+                "
+            >
+              <div
+                className="
+                    bg-primary
+                    absolute
+                    opacity-30
+                    w-full
+                    h-full
+                    inset-0
+                    group-hover:opacity-0
+                    transition
+                    duration-500
+                    ease-in-out
+                  "
+              ></div>
+            </div>
+            <div className="font-bold">Patricia Vianney</div>
+            <div>Public Relations Director</div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div
+              className="
+                  relative
+                  w-56
+                  h-64
+                  shadow-lg
+                  bg-vannes bg-cover bg-left-top
+                  cursor-pointer
+                  group
+                  mb-5
+                "
+            >
+              <div
+                className="
+                    bg-primary
+                    absolute
+                    opacity-30
+                    w-full
+                    h-full
+                    inset-0
+                    group-hover:opacity-0
+                    transition
+                    duration-500
+                    ease-in-out
+                  "
+              ></div>
+            </div>
+            <div className="font-bold">Stefan Rafael</div>
+            <div>Product Director</div>
+          </div>
+        </div>
+        <div className="grid grid-cols-5 gap-x-3">
+          <div className="flex flex-col items-center">
+            <div
+              className="
+                  relative
+                  w-56
+                  h-64
+                  shadow-lg
+                  bg-jodi bg-cover bg-center
+                  cursor-pointer
+                  group
+                  mb-5
+                "
+            >
+              <div
+                className="
+                    bg-primary
+                    absolute
+                    opacity-30
+                    w-full
+                    h-full
+                    inset-0
+                    group-hover:opacity-0
+                    transition
+                    duration-500
+                    ease-in-out
+                  "
+              ></div>
+            </div>
+            <div className="font-bold">Jonathan Andika</div>
+            <div>Operations Director</div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div
+              className="
+                  relative
+                  w-56
+                  h-64
+                  shadow-lg
+                  bg-utap bg-cover bg-bottom
+                  cursor-pointer
+                  group
+                  mb-5
+                "
+            >
+              <div
+                className="
+                    bg-primary
+                    absolute
+                    opacity-30
+                    w-full
+                    h-full
+                    inset-0
+                    group-hover:opacity-0
+                    transition
+                    duration-500
+                    ease-in-out
+                  "
+              ></div>
+            </div>
+            <div className="font-bold">Benedict Ronaldo Gustaf</div>
+            <div>Event Director</div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div
+              className="
+                  relative
+                  w-56
+                  h-64
+                  shadow-lg
+                  bg-lim bg-cover
+                  cursor-pointer
+                  group
+                  mb-5
+                "
+            >
+              <div
+                className="
+                    bg-primary
+                    absolute
+                    opacity-30
+                    w-full
+                    h-full
+                    inset-0
+                    group-hover:opacity-0
+                    transition
+                    duration-500
+                    ease-in-out
+                  "
+              ></div>
+            </div>
+            <div className="font-bold">Michael Lim</div>
+            <div>Advisor</div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div
+              className="
+                  relative
+                  w-56
+                  h-64
+                  shadow-lg
+                  bg-kelvin bg-cover bg-center
+                  cursor-pointer
+                  group
+                  mb-5
+                "
+            >
+              <div
+                className="
+                    bg-primary
+                    absolute
+                    opacity-30
+                    w-full
+                    h-full
+                    inset-0
+                    group-hover:opacity-0
+                    transition
+                    duration-500
+                    ease-in-out
+                  "
+              ></div>
+            </div>
+            <div className="font-bold">Kelvin Hartono</div>
+            <div>Advisor</div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div
+              className="
+                  relative
+                  w-56
+                  h-64
+                  shadow-lg
+                  bg-vannes bg-cover bg-left-top
+                  cursor-pointer
+                  group
+                  mb-5
+                "
+            >
+              <div
+                className="
+                    bg-primary
+                    absolute
+                    opacity-30
+                    w-full
+                    h-full
+                    inset-0
+                    group-hover:opacity-0
+                    transition
+                    duration-500
+                    ease-in-out
+                  "
+              ></div>
+            </div>
+            <div className="font-bold">Michelle Karen</div>
+            <div>Advisor</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
